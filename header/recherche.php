ab31e66d792400b1b0b1ee0f082005d9b99670d9
david <david.albert@insa-rouen.fr>
2019-04-10 21:59:44 +0200

    Initial commit

---
+++ header/recherche.php
@@ -0,0 +1,24 @@
+<?php
+
+$NB_AFFICHAGE = 2;
+
+$donnees = ["Rion","Marion","Dav","Rithon","Henri","Max","Maxou"];
+shuffle($donnees);
+
+$term = $_GET['term'];
+
+/*$requete = $bdd->prepare('SELECT * FROM membres WHERE pseudo LIKE :term'); 
+$requete->execute(array('term' => '%'.$term.'%'));
+
+$array = array();
+
+while($donnee = $requete->fetch()) // on effectue une boucle pour obtenir les données
+for($i=0;$i<$NB_AFFICHAGE;$i++)
+{
+    array_push($array, $donnee['pseudo']); // et on ajoute celles-ci à notre tableau
+}
+*/
+
+echo json_encode($donnees); 
+
+?>
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Connexion</title>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <meta http-equiv="Content-Style-Type" content="text/css" />
+  <link rel="stylesheet" type="text/css" href="style.css" media="all" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+  <?php
+  include("header/header.html");
+  ?>
+  <div class="main-content">
+    <h1> Site bière</h1>
+  </div>
+</body>
+
+</html>
