a6b557e0163d3db26b91add1de04efbabc8b846c
david <david.albert@insa-rouen.fr>
2019-04-14 12:53:33 +0200

    Mise a jour style pour meilleure reutilisabilité

--- header/header.php
+++ header/header.php
@@ -22,18 +22,18 @@
 </script>
 
 <header class>
-    <div class="header_leftPart">
+    <div class="header-block leftSide">
         <?php
-        // On affiche la photo de profile
+        // On affiche la photo de profil
         if (isset($_SESSION['util'])) { 
             $_SESSION['util']->afficherPhoto(40,40);
         }
         ?>
     </div>
-    <div class="header_rightPart">
-        <button class=" button "> <span class=" glyphicon glyphicon-log-out"> Deconnect </span> </button>
+    <div class="header-block rightSide">
+        <button class="button-deconnect"> <span class=" glyphicon glyphicon-log-out"> Deconnect </span> </button>
     </div>
-    <div class="header_centerPart">
+    <div class="header-block centerPart">
         <form>
             <input class="max-width" type="text" id="recherche" />
         </form>
