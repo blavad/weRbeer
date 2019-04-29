ff605d3abe3b7824593740d31f1cc60f8701a5d0
Marion <marion.schaeffer@insa-rouen.fr>
2019-04-29 18:47:57 +0200

    Barre de navigation

---
+++ navbar/navbar.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" type="text/css" href="style.css" media="all" />
+<link rel="stylesheet" type="text/css" href="navbar/stylenav.css" media="all" />
+<nav>
+	<ul>
+		<li> <a href="actu.php">Actualités</a> </li>
+		<li> <a href="profil.php">Mon profil</a> </li>
+		<li> <a href="cave.php">Ma cave</a> </li>
+		<li> <a href="amis.php">Mes amis</a> </li>
+		<li> <a href="param.php">Paramètres</a> </li>
+	</ul>
+</nav>
