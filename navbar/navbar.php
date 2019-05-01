19eea42de506b881f3e9ddf45f011799e5dca0cd
Marion <marion.schaeffer@insa-rouen.fr>
2019-05-01 21:37:46 +0200

    modif hover

--- navbar/navbar.php
+++ navbar/navbar.php
@@ -5,7 +5,7 @@
 		<li> <a href="index.php">Actualités</a> </li>
 		<li> <a href="profil.php?id=<?php echo $_SESSION['util']->getId();?> ">Mon profil</a> </li>
 		<li> <a href="cave.php">Ma cave</a> </li>
-		<li> <a href="amis.php">Mes amis</a> </li>
+		<li> <a href="listeamis.php">Mes amis</a> </li>
 		<li> <a href="param.php">Paramètres</a> </li>
 	</ul>
 </nav>
